6bd12be3c11632352b9a54447eee74218fe446ea
Lolster24Vito <vitomir.hardi@gmail.com>
2022-05-30 19:18:20 +0200

    Contact us validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+
+    validates_presence_of :name
+    validates_presence_of :email
+    validates :email, format: { with: Devise.email_regexp }
+    validates_presence_of :message
+
 end
