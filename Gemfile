7410ee447ca0a4ed6cd5d4251550d0ffbed8336e
Lolster24Vito <vitomir.hardi@gmail.com>
2022-05-31 17:57:59 +0200

    Still not resolved,having trouble with a bug in the terminal  "Aborted (core dumped)"

--- Gemfile
+++ Gemfile
@@ -78,6 +78,7 @@ gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.2'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem 'rspec-rails', '~> 3.8'
 end
 
 
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+    describe 'create' do
+        it 'successfully creates a new user' do
+            user = User.create(username: "testuser",
+                encrypted_password:"asdfghj",
+                 email: "test@gmail.com",
+                 created_at:DateTime.now , updated_at: DateTime.now )
+       
+            expect(user.last.username).to eq("testuser")
+        end
+    end
+end
